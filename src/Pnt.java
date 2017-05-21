5e876f5a766df22c538dfb0592850cb065ef43d2
Dmitry Abramenko <abramenko@a2design.biz>
2017-05-22 01:09:28 +0600

    added construsctor to Point class

--- src/Pnt.java
+++ src/Pnt.java
@@ -1,4 +1,18 @@
 
 public class Pnt {
-    
+
+    private double[] coordinates;
+
+    public Pnt (double... coords) {
+        coordinates = new double[coords.length];
+        System.arraycopy(coords, 0, coordinates, 0, coords.length);
+    }
+
+    public double coord (int i) {
+        return this.coordinates[i];
+    }
+
+    public int dimension () {
+        return coordinates.length;
+    }
 }
