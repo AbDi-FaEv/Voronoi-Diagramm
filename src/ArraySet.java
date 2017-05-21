6bca8d1d3f01c0f95f1a890910ed86e1e5b43a5a
Dmitry Abramenko <abramenko@a2design.biz>
2017-05-22 03:34:04 +0600

    added constructors to ArraySet

--- src/ArraySet.java
+++ src/ArraySet.java
@@ -1,10 +1,25 @@
 import java.util.AbstractSet;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 
 public class ArraySet<E> extends AbstractSet<E> {
 
     private ArrayList<E> items;
+
+    public ArraySet () {
+        this(3);
+    }
+
+    public ArraySet (int initialCapacity) {
+        items  = new ArrayList<E>(initialCapacity);
+    }
+
+    public ArraySet (Collection<? extends E> collection) {
+        items = new ArrayList<E>(collection.size());
+        for (E item: collection)
+            if (!items.contains(item)) items.add(item);
+    }
 
     @Override
     public boolean add(E item) {
