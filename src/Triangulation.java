b135c42e483752f417eb3971efba49cfbe0bdefd
Dmitry Abramenko <abramenko@a2design.biz>
2017-05-23 16:59:18 +0600

    added extendig fot Triangulation Class

--- src/Triangulation.java
+++ src/Triangulation.java
@@ -1,5 +1,30 @@
+import java.util.AbstractSet;
+import java.util.Iterator;
+
 /**
  * Created by dimad on 22.05.2017.
  */
-public class Triangulation {
+public class Triangulation  extends AbstractSet<Triangle> {
+    private Triangle mostRecent = null;
+    private Graph<Triangle> triGraph;
+
+    public Triangulation (Triangle triangle) {
+        triGraph = new Graph<Triangle>();
+        triGraph.add(triangle);
+        mostRecent = triangle;
+    }
+    @Override
+    public Iterator<Triangle> iterator () {
+        return triGraph.nodeSet().iterator();
+    }
+
+    @Override
+    public int size () {
+        return triGraph.nodeSet().size();
+    }
+
+    @Override
+    public String toString () {
+        return "Triangulation with " + size() + " triangles";
+    }
 }
