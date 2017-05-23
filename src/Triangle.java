86e075b2dbe6b7e0c4db5dfb3dfe15b40991aedc
Dmitry Abramenko <abramenko@a2design.biz>
2017-05-23 11:04:31 +0600

    override basic methods in Triangle class

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,6 +1,44 @@
+import java.util.Iterator;
+
 /**
  * Created by dimad on 22.05.2017.
  */
 public class Triangle extends ArraySet<Pnt>  {
+    private int idNumber;
+    private Pnt circumcenter = null;
 
+    private static int idGenerator = 0;
+    public static boolean moreInfo = false;
+
+    
+
+    @Override
+    public String toString () {
+        if (!moreInfo) return "Triangle" + idNumber;
+        return "Triangle" + idNumber + super.toString();
+    }
+    @Override
+    public boolean add (Pnt vertex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<Pnt> iterator () {
+        return new Iterator<Pnt>() {
+            private Iterator<Pnt> it = Triangle.super.iterator();
+            public boolean hasNext() {return it.hasNext();}
+            public Pnt next() {return it.next();}
+            public void remove() {throw new UnsupportedOperationException();}
+        };
+    }
+
+    @Override
+    public int hashCode () {
+        return (int)(idNumber^(idNumber>>>32));
+    }
+
+    @Override
+    public boolean equals (Object o) {
+        return (this == o);
+    }
 }
