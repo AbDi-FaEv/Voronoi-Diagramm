2d73fa0605cc087951593e08b5faf86c20f9905b
Dmitry Abramenko <abramenko@a2design.biz>
2017-05-26 11:44:04 +0600

    added setter to Triangulation class

--- src/Triangulation.java
+++ src/Triangulation.java
@@ -1,5 +1,4 @@
-import java.util.AbstractSet;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Created by dimad on 22.05.2017.
@@ -27,4 +26,37 @@ public class Triangulation  extends AbstractSet<Triangle> {
     public String toString () {
         return "Triangulation with " + size() + " triangles";
     }
+    public boolean contains (Object triangle) {
+        return triGraph.nodeSet().contains(triangle);
+    }
+
+    public Triangle neighborOpposite (Pnt site, Triangle triangle) {
+        if (!triangle.contains(site))
+            throw new IllegalArgumentException("Bad vertex; not in triangle");
+        for (Triangle neighbor: triGraph.neighbors(triangle)) {
+            if (!neighbor.contains(site)) return neighbor;
+        }
+        return null;
+    }
+
+    public Set<Triangle> neighbors(Triangle triangle) {
+        return triGraph.neighbors(triangle);
+    }
+
+    public List<Triangle> surroundingTriangles (Pnt site, Triangle triangle) {
+        if (!triangle.contains(site))
+            throw new IllegalArgumentException("Site not in triangle");
+        List<Triangle> list = new ArrayList<Triangle>();
+        Triangle start = triangle;
+        Pnt guide = triangle.getVertexButNot(site);        // Affects cw or ccw
+        while (true) {
+            list.add(triangle);
+            Triangle previous = triangle;
+            triangle = this.neighborOpposite(guide, triangle); // Next triangle
+            guide = previous.getVertexButNot(site, guide);     // Update guide
+            if (triangle == start) break;
+        }
+        return list;
+    }
+
 }
