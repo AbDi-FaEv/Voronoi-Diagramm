38e9b4378eecf5c556958f9f06acac5b727d79d6
Dmitry Abramenko <abramenko@a2design.biz>
2017-05-26 13:44:21 +0600

    added extending for main class

--- src/Voronoi.java
+++ src/Voronoi.java
@@ -1,5 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 /**
  * Created by dimad on 23.05.2017.
  */
-public class Voronoi {
+public class Voronoi extends javax.swing.JApplet
+        implements Runnable, ActionListener, MouseListener {
+    public void init () {
+
+    }
+    public void run () {
+
+    }
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {}
 }
+
