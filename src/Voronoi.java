4988c34fbdf102c892231e3c318a65d83e6f33f0
Dmitry Abramenko <abramenko@a2design.biz>
2017-05-26 13:50:52 +0600

    added constructor to VoronoiPanel

--- src/Voronoi.java
+++ src/Voronoi.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
@@ -51,6 +52,31 @@ public class Voronoi extends javax.swing.JApplet
         private static int initialSize = 10000;
         private Graphics g;
         private Random random = new Random();
+
+        public VoronoiPanel(Voronoi controller) {
+            this.controller = controller;
+            initialTriangle = new Triangle(
+                    new Pnt(-initialSize, -initialSize),
+                    new Pnt( initialSize, -initialSize),
+                    new Pnt(           0,  initialSize));
+            dt = new Triangulation(initialTriangle);
+            colorTable = new HashMap<Object, Color>();
+        }
+
+        public void addSite(Pnt point) {
+            dt.delaunayPlace(point);
+        }
+
+        public void clear() {
+            dt = new Triangulation(initialTriangle);
+        }
+
+        private Color getColor (Object item) {
+            if (colorTable.containsKey(item)) return colorTable.get(item);
+            Color color = new Color(Color.HSBtoRGB(random.nextFloat(), 1.0f, 1.0f));
+            colorTable.put(item, color);
+            return color;
+        }
     }
 }
 
